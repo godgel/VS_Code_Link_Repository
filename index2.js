a74ebc3535a4c17e6eed38a4b64414df73d86471
godgel <83522575+godgel@users.noreply.github.com>
2022-01-23 18:37:47 +0900

    Add files via upload
    JavScript_02

---
+++ index2.js
@@ -0,0 +1,14 @@
+let name = 'Nam_Seok';
+let age = 58;
+let $Back_Tic = 'My Dream is This....';
+
+console.log(name);
+console.log(age);
+console.log($Back_Tic);
+//console.log(name + age + $Back_Tic);
+
+console.log(` What's your Dream...? ${$Back_Tic} `);
+
+let _char$print = '"반갑습니다....!!!"';
+
+console.log('정말 정말  ' + `최고로, 억수로 ${_char$print}`);
